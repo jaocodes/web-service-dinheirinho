25321dcd22a21d3a349d7f3630131cd23740f4fe
jaocodes <jhonskaz@gmail.com>
2025-02-02 20:43:32 -0300

    adiciona validação para variáveis de ambiente do projeto

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(['dev', 'prod', 'test']).default('dev'),
+    PORT: z.coerce.number().default(3333),
+    SUPER_SECRET_JWT: z.string(),
+    DATABASE_URL: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+    console.error('Environment validation error:', _env.error.format())
+
+    throw new Error('Environment validation error')
+}
+
+export const env = _env.data
