958424feb308c7a27d11900b7d723007beb7cf28
jaocodes <jhonskaz@gmail.com>
2025-02-02 20:44:41 -0300

    adiciona instancia do fastify e inicializa o servidor http

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { registerUser } from './users/register'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'API Dinheirinho',
+      description: 'Documentação da API Dinheirinho',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(registerUser)
