79ad2103d8fb64a359288ae71a63a9710acbbbde
jaocodes <jhonskaz@gmail.com>
2025-02-11 07:56:25 -0300

    refatora testes de totalAmount para usar função makeAuthenticateUser e incluir token JWT

--- src/http/controllers/accounts/get-total-amount.spec.ts
+++ src/http/controllers/accounts/get-total-amount.spec.ts
@@ -14,6 +14,7 @@ import type { z } from 'zod'
 import { makeUser } from 'test/factories/makeUser'
 import { makeAccount } from 'test/factories/makeAccount'
 import type { createTransactionBodySchema } from '../transactions/create-transaction'
+import { makeAuthenticateUser } from 'test/factories/makeAuthenticateUser'
 
 describe('(e2e) GET /totalAmount/:userId', () => {
   beforeAll(async () => {
@@ -33,67 +34,88 @@ describe('(e2e) GET /totalAmount/:userId', () => {
 
   const createTransaction = async (
     transaction: z.infer<typeof createTransactionBodySchema>,
+    token: string,
   ) => {
-    await request(app.server).post('/transactions').send(transaction)
+    await request(app.server)
+      .post('/transactions')
+      .set('Authorization', `Bearer ${token}`)
+      .send(transaction)
   }
 
   it('deve retornar o balanço do mês passado com tipo SALDO_ATÉ_O_FIM_DO_MÊS', async () => {
     vi.setSystemTime(new Date(2025, 1, 9, 10, 0, 0))
 
-    const user = await makeUser()
+    const { userInput, userCreated } = await makeUser()
+    const { token } = await makeAuthenticateUser(app, userInput)
     const account = await makeAccount({
-      userId: user.id,
+      userId: userCreated.id,
       initialBalance: 125 * 100,
     })
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 10 * 100,
-      description: 'Receita efetivada em dezembro',
-      dueDate: new Date(2024, 11, 15),
-      type: 'INCOME',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 3 * 100,
-      description: 'Despesa efetivada em dezembro',
-      dueDate: new Date(2024, 11, 20),
-      type: 'EXPENSE',
-      effectived: true,
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 10 * 100,
+        description: 'Receita efetivada em dezembro',
+        dueDate: new Date(2024, 11, 15),
+        type: 'INCOME',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 3 * 100,
+        description: 'Despesa efetivada em dezembro',
+        dueDate: new Date(2024, 11, 20),
+        type: 'EXPENSE',
+        effectived: true,
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 5 * 100,
-      description: 'Despesa efetivada em janeiro',
-      dueDate: new Date(2025, 0, 10),
-      type: 'EXPENSE',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 200,
-      description: 'Despesa não efetivada em janeiro',
-      dueDate: new Date(2025, 0, 15),
-      type: 'EXPENSE',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 5 * 100,
+        description: 'Despesa efetivada em janeiro',
+        dueDate: new Date(2025, 0, 10),
+        type: 'EXPENSE',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 200,
+        description: 'Despesa não efetivada em janeiro',
+        dueDate: new Date(2025, 0, 15),
+        type: 'EXPENSE',
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 1000,
-      description: 'Receita efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 3),
-      type: 'INCOME',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 1000,
+        description: 'Receita efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 3),
+        type: 'INCOME',
+      },
+      token,
+    )
 
     const response = await request(app.server)
-      .get(`/totalAmount/${user.id}`)
+      .get(`/totalAmount/${userCreated.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .query({ month: '2025-01' })
       .send()
 
@@ -107,77 +129,101 @@ describe('(e2e) GET /totalAmount/:userId', () => {
   it('deve retornar o balanço do mês atual com tipo SALDO_ATUAL_EM_CONTAS', async () => {
     vi.setSystemTime(new Date(2025, 1, 9, 10, 0, 0))
 
-    const user = await makeUser()
+    const { userInput, userCreated } = await makeUser()
+    const { token } = await makeAuthenticateUser(app, userInput)
+
     const account = await makeAccount({
-      userId: user.id,
+      userId: userCreated.id,
       initialBalance: 125 * 100,
     })
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 5 * 100,
-      description: 'Receita efetivada em janeiro',
-      dueDate: new Date(2025, 0, 20),
-      type: 'INCOME',
-      effectived: true,
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 5 * 100,
+        description: 'Receita efetivada em janeiro',
+        dueDate: new Date(2025, 0, 20),
+        type: 'INCOME',
+        effectived: true,
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 3 * 100,
-      description: 'Despesa não efetivada em janeiro',
-      dueDate: new Date(2025, 0, 25),
-      type: 'EXPENSE',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 3 * 100,
+        description: 'Despesa não efetivada em janeiro',
+        dueDate: new Date(2025, 0, 25),
+        type: 'EXPENSE',
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 7 * 100,
-      description: 'Despesa efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 3),
-      type: 'EXPENSE',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 10 * 100,
-      description: 'Receita efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 6),
-      type: 'INCOME',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 4 * 100,
-      description: 'Despesa não efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 15),
-      type: 'EXPENSE',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 7 * 100,
+        description: 'Despesa efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 3),
+        type: 'EXPENSE',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 10 * 100,
+        description: 'Receita efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 6),
+        type: 'INCOME',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 4 * 100,
+        description: 'Despesa não efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 15),
+        type: 'EXPENSE',
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 5 * 100,
-      description: 'Despesa efetivada em março',
-      dueDate: new Date(2025, 2, 5),
-      type: 'EXPENSE',
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 6 * 100,
-      description: 'Receita não efetivada em março',
-      dueDate: new Date(2025, 2, 10),
-      type: 'INCOME',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 5 * 100,
+        description: 'Despesa efetivada em março',
+        dueDate: new Date(2025, 2, 5),
+        type: 'EXPENSE',
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 6 * 100,
+        description: 'Receita não efetivada em março',
+        dueDate: new Date(2025, 2, 10),
+        type: 'INCOME',
+      },
+      token,
+    )
 
     const response = await request(app.server)
-      .get(`/totalAmount/${user.id}`)
+      .get(`/totalAmount/${userCreated.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .query({ month: '2025-02' })
       .send()
 
@@ -190,68 +236,87 @@ describe('(e2e) GET /totalAmount/:userId', () => {
 
   it('deve retornar o balanço do mês futuro com tipo SALDO_PREVISTO', async () => {
     vi.setSystemTime(new Date(2025, 1, 9, 10, 0, 0))
+    const { userInput, userCreated } = await makeUser()
+    const { token } = await makeAuthenticateUser(app, userInput)
 
-    const user = await makeUser()
     const account = await makeAccount({
-      userId: user.id,
+      userId: userCreated.id,
       initialBalance: 125 * 100, // 12500
     })
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 10 * 100,
-      description: 'Receita efetivada em janeiro',
-      dueDate: new Date(2025, 0, 20),
-      type: 'INCOME',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 200,
-      description: 'Despesa não efetivada em janeiro',
-      dueDate: new Date(2025, 0, 25),
-      type: 'EXPENSE',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 10 * 100,
+        description: 'Receita efetivada em janeiro',
+        dueDate: new Date(2025, 0, 20),
+        type: 'INCOME',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 200,
+        description: 'Despesa não efetivada em janeiro',
+        dueDate: new Date(2025, 0, 25),
+        type: 'EXPENSE',
+      },
+      token,
+    )
 
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 800,
-      description: 'Despesa efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 6),
-      type: 'EXPENSE',
-      effectived: true,
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 300,
-      description: 'Receita não efetivada em fevereiro',
-      dueDate: new Date(2025, 1, 15),
-      type: 'INCOME',
-    })
-
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 1500,
-      description: 'Receita efetivada em março',
-      dueDate: new Date(2025, 2, 10),
-      type: 'INCOME',
-    })
-    await createTransaction({
-      userId: user.id,
-      accountId: account.id,
-      amount: 400,
-      description: 'Despesa não efetivada em março',
-      dueDate: new Date(2025, 2, 25),
-      type: 'EXPENSE',
-    })
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 800,
+        description: 'Despesa efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 6),
+        type: 'EXPENSE',
+        effectived: true,
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 300,
+        description: 'Receita não efetivada em fevereiro',
+        dueDate: new Date(2025, 1, 15),
+        type: 'INCOME',
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 1500,
+        description: 'Receita efetivada em março',
+        dueDate: new Date(2025, 2, 10),
+        type: 'INCOME',
+      },
+      token,
+    )
+    await createTransaction(
+      {
+        userId: userCreated.id,
+        accountId: account.id,
+        amount: 400,
+        description: 'Despesa não efetivada em março',
+        dueDate: new Date(2025, 2, 25),
+        type: 'EXPENSE',
+      },
+      token,
+    )
 
     const response = await request(app.server)
-      .get(`/totalAmount/${user.id}`)
+      .get(`/totalAmount/${userCreated.id}`)
+      .set('Authorization', `Bearer ${token}`)
       .query({ month: '2025-03' })
       .send()
 
