c0087efd608c18cf760b83b1aa8dcbf8a3fed1bf
jaocodes <jhonskaz@gmail.com>
2025-02-17 16:55:33 -0300

    adiciona lógica para calcular datas e valores de faturas abertas e fechadas no endpoint de busca de cartões de crédito

--- src/http/controllers/credit-card/fetch-credit-cards.ts
+++ src/http/controllers/credit-card/fetch-credit-cards.ts
@@ -3,7 +3,7 @@ import { verifyJWT } from '../hooks/verify-jwt'
 import { z } from 'zod'
 import { prisma } from '@/prisma-client'
 
-const fetchCreditCardsResponseSchema = z.object({
+export const fetchCreditCardsResponseSchema = z.object({
   creditCards: z.array(
     z.object({
       id: z.string().uuid(),
@@ -14,9 +14,47 @@ const fetchCreditCardsResponseSchema = z.object({
         id: z.string().uuid(),
         name: z.string(),
       }),
+      openedInvoiceDate: z.coerce.date(),
+      openedInvoiceAmount: z.number().int(),
+      closedInvoiceDate: z.coerce.date(),
+      closedInvoiceAmount: z.number().int(),
     }),
   ),
 })
+
+function getInvoicesDate(closingDay: number, dueDay: number) {
+  const currentDate = new Date()
+
+  if (currentDate.getDate() > closingDay) {
+    const openedInvoiceDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth() + 1,
+      dueDay,
+    )
+
+    const closedInvoiceDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      dueDay,
+    )
+
+    return { openedInvoiceDate, closedInvoiceDate }
+  }
+
+  const openedInvoiceDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    dueDay,
+  )
+
+  const closedInvoiceDate = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth() - 1,
+    dueDay,
+  )
+
+  return { openedInvoiceDate, closedInvoiceDate }
+}
 
 export const fetchCreditCards: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -41,6 +79,8 @@ export const fetchCreditCards: FastifyPluginAsyncZod = async (app) => {
           name: true,
           initialLimit: true,
           currentLimit: true,
+          dueDay: true,
+          closingDay: true,
           account: {
             select: {
               id: true,
@@ -50,7 +90,50 @@ export const fetchCreditCards: FastifyPluginAsyncZod = async (app) => {
         },
       })
 
-      return reply.status(200).send({ creditCards })
+      const creditCardWithOpenInvoices = await Promise.all(
+        creditCards.map(async (creditCard) => {
+          const { openedInvoiceDate, closedInvoiceDate } = getInvoicesDate(
+            creditCard.closingDay,
+            creditCard.dueDay,
+          )
+
+          const openedCreditExpenses = await prisma.transaction.aggregate({
+            where: {
+              creditCardId: creditCard.id,
+              userId,
+              invoiceDate: openedInvoiceDate,
+            },
+            _sum: {
+              amount: true,
+            },
+          })
+
+          const closedCreditExpenses = await prisma.transaction.aggregate({
+            where: {
+              creditCardId: creditCard.id,
+              userId,
+              invoiceDate: closedInvoiceDate,
+            },
+            _sum: {
+              amount: true,
+            },
+          })
+
+          return {
+            id: creditCard.id,
+            name: creditCard.name,
+            initialLimit: creditCard.initialLimit,
+            currentLimit: creditCard.currentLimit,
+            account: creditCard.account,
+            openedInvoiceDate,
+            openedInvoiceAmount: openedCreditExpenses._sum.amount || 0,
+            closedInvoiceDate,
+            closedInvoiceAmount: closedCreditExpenses._sum.amount || 0,
+          }
+        }),
+      )
+
+      return reply.status(200).send({ creditCards: creditCardWithOpenInvoices })
     },
   )
 }
