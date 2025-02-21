ef34f180b45868ad9c7b3b0287e1729618620044
jaocodes <jhonskaz@gmail.com>
2025-02-20 23:49:16 -0300

    adiciona testes e2e para efetivar transações

---
+++ src/http/controllers/transactions/effective-transaction.spec.ts
@@ -0,0 +1,47 @@
+import { app } from '@/app'
+import request from 'supertest'
+import { makeAccount } from 'test/factories/makeAccount'
+import { makeAuthenticateUser } from 'test/factories/makeAuthenticateUser'
+import { makeTransaction } from 'test/factories/makeTransaction'
+import { makeUser } from 'test/factories/makeUser'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('(e2e) POST /transactions/:transactionId/effective', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to effective a transaction', async () => {
+        const { userInput, userCreated } = await makeUser()
+        const { token } = await makeAuthenticateUser(app, userInput)
+
+        const account = await makeAccount({
+            userId: userCreated.id,
+            initialBalance: 125 * 100,
+        })
+
+
+        const transaction = await makeTransaction({
+            userId: userCreated.id,
+            accountId: account.id,
+            amount: 25 * 100,
+            dueDate: new Date(2025, 1, 6),
+            type: 'EXPENSE',
+            categoryId: 14,
+        })
+
+        const response = await request(app.server)
+            .patch(`/transactions/${transaction.id}/effective`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({})
+
+        expect(response.status).toEqual(204)
+
+
+
+    })
+})
