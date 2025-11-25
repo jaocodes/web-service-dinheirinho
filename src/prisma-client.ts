83747c3dea551676aecf0c12ff12846919c728c3
jaocodes <jhonskaz@gmail.com>
2025-11-24 23:35:04 -0300

    refactor: update dependencies and configuration for Prisma and Bun
    - Changed the test command in package.json to use Vitest.
    - Updated Prisma version to 7 and adjusted related dependencies.
    - Created a new prisma.config.ts file for configuration management.
    - Modified schema.prisma to use the new Prisma client generator.
    - Refactored prisma-client.ts to utilize the new @prisma/adapter-pg.

--- src/prisma-client.ts
+++ src/prisma-client.ts
@@ -1,9 +1,17 @@
-import { env } from '@/env'
-import { PrismaClient } from '@prisma/client'
+// import { env } from '@/env'
+// import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  log:
-    env.NODE_ENV === 'dev'
-      ? ['query', 'info', 'warn', 'error']
-      : ['warn', 'error'],
-})
+// export const prisma = new PrismaClient({
+//   log:
+//     env.NODE_ENV === 'dev'
+//       ? ['query', 'info', 'warn', 'error']
+//       : ['warn', 'error'],
+// })
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from './generated/prisma/client';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+});
+export const prisma = new PrismaClient({ adapter });
